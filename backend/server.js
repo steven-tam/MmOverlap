4f7db00823783c93774e67d7ccc7392fd2d76a1d
Steven Tam <steven.tam123@gmail.com>
2024-02-01 23:58:31 -0600

    implemented express

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 // fileName : server.js 
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
 })
 
 const PORT = 3001
