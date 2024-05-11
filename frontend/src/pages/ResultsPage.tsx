a616ac7106994c8b9f2ea5a72e972c70af34f447
Steven Tam <steven.tam123@gmail.com>
2024-05-10 21:48:33 -0500

    Completely dropping the tab feature
    its too complicated :dizzy_face:

--- frontend/src/pages/ResultsPage.tsx
+++ frontend/src/pages/ResultsPage.tsx
@@ -2,8 +2,7 @@ import { useEffect, useState } from 'react'
 import ChartResults from '../components/ChartResults';
 import CoursesData from "../../../jsonGenerator/allCourses.json";
 import ProgramData from "../../../jsonGenerator/allMajors.json";
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+
 
 type Program = {
     id: number; // Index in search bar, see handleProgramClick / handleKeyDown
@@ -20,14 +19,51 @@ type Course = {
     };
 };
 
+const data = [
+    {
+      label: "HTML",
+      value: "html",
+      desc: `It really matters and then like it really doesn't matter.
+      What matters is the people who are sparked by it. And the people 
+      who are like offended by it, it doesn't matter.`,
+    },
+    {
+      label: "React",
+      value: "react",
+      desc: `Because it's about motivating the doers. Because I'm here
+      to follow my dreams and inspire other people to follow their dreams, too.`,
+    },
+    {
+      label: "Vue",
+      value: "vue",
+      desc: `We're not always in the position that we want to be at.
+      We're constantly growing. We're constantly making mistakes. We're
+      constantly trying to express ourselves and actualize our dreams.`,
+    },
+    {
+      label: "Angular",
+      value: "angular",
+      desc: `Because it's about motivating the doers. Because I'm here
+      to follow my dreams and inspire other people to follow their dreams, too.`,
+    },
+    {
+      label: "Svelte",
+      value: "svelte",
+      desc: `We're not always in the position that we want to be at.
+      We're constantly growing. We're constantly making mistakes. We're
+      constantly trying to express ourselves and actualize our dreams.`,
+    },
+  ];
+
+
 
 function ResultPage() {
   let program_selected = sessionStorage.getItem("program_selected");
   let courses_selected = sessionStorage.getItem("courses_selected");
   const yourCourses = courses_selected != null ? JSON.parse(courses_selected) : [] //Fixes string or null type error when parsing
   const yourProgram = program_selected != null ? program_selected : "Undecided"
-  const programDataTyped = ProgramData as Program[] //type casts programData
-  const coursesDataTyped = CoursesData as Course[] //type casts coursesData
+  const allPrograms = ProgramData as Program[] //type casts programData
+  const allCourses = CoursesData as Course[] //type casts coursesData
   var creditCounter = 0;
   
   console.log("Program Selected:", yourProgram)
@@ -37,7 +73,7 @@ function ResultPage() {
     totalCredits = 0 // resets counter when you revise your courses
     for (const courseId of courseIds) {
         // Match course ID with the courses data
-        for (const course of CoursesData) { // for of statements invokes the Symbol.iterator. Ignore error for now
+        for (const course of allCourses) { // for of statements invokes the Symbol.iterator. Ignore error for now
             if (course.id.startsWith(courseId.substring(0, 7))) {
                 totalCredits += course.credits.numberOfCredits;
                 break; // Exit the inner loop after finding the course
@@ -47,6 +83,7 @@ function ResultPage() {
     return totalCredits;
 }
 
+//Inactive but will be used for refactoring
 function validateCourses(subRuleArray:any){
     //This function checks if yourCourses meets the subRules of a Core
     for(var i in subRuleArray){
@@ -111,7 +148,7 @@ function createChecklist(requirements: any, yourCourses: string[]){
 
             for(var i in subRules){
                 const subRule = subRules[i]
-                const subName = subRule['name'] // Ex: Economics
+                const subName = subRule['name'] ? subRule['name'] : "Unnamed" // Ex: Economics
                 const subCondition = subRule['condition']
                 const courseArray = subRule['value']['values'] // Ex: [{"logic": "or","value": ["8257721","0099201","0062811","8019831"]}, ...]
         
@@ -161,7 +198,7 @@ function createChecklist(requirements: any, yourCourses: string[]){
 }
 
 function checkRequirements(yourProgram: string){
-    const getProgramObj = yourProgram != 'Undecided' ? programDataTyped.find(p => p.catalogDisplayName == yourProgram): false 
+    const getProgramObj = yourProgram != 'Undecided' ? allPrograms.find(p => p.catalogDisplayName == yourProgram): false 
 
     if (getProgramObj){
         const requirements = getProgramObj['requisites']['requisitesSimple'] // type ex: [{...},{...}]
@@ -183,26 +220,10 @@ checkRequirements(yourProgram)
 return (
   <div> 
     <p>Total Credits: {calculateTotalCredits(yourCourses, creditCounter)}</p>
-    <Tabs>
-    <TabList>
-      <Tab>{yourProgram}</Tab>
-      <Tab>Title 2</Tab>
-    </TabList>
-
-    <TabPanel>
-      <h2>Any content 1</h2>
-    </TabPanel>
-    <TabPanel>
-      <h2>Any content 2</h2>
-    </TabPanel>
-  </Tabs>
-
     <ChartResults 
       selectedProgram={yourProgram} 
       selectedCourses={yourCourses}
     />
-
-
   </div>
 )
 }
